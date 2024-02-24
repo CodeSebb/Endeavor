fc38646293450d01734e1a5fe2c3dcf0fb4a730e
CodeSebb <SebastianSalvadorSanchez@gmail.com>
2024-02-24 12:37:40 -0800

    First Upload

---
+++ app/Sign In/page.tsx
@@ -0,0 +1,11 @@
+
+
+export default function SignIn () {
+    return(
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <div>
+                <h1> Sign In</h1>
+            </div>
+        </main>
+    );
+}
